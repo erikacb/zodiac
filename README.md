7dde0e9036aa85e5b068cad228c1994cc9553140
Erika Bueno <erika@totalcommit.com>
2017-03-22 02:09:10 -0300

    Show category name and language name

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Zodiac
 
-A random horoscope generator.
+A random horoscope generator created by Erika Bueno.
 
 * Ruby version: 2.3.0
 
--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,6 +1,37 @@
 class SentencesController < ApplicationController
   before_action :set_sentence, only: [:show, :edit, :update, :destroy]
 
+  def show_category_name(category_number)
+
+    case category_number
+    when 0
+      return "Family"
+    when 1
+      return "Love"
+    when 2
+      return "Health"
+    when 3
+      return "Career"
+    else
+      return "Money"
+    end
+  end
+
+  helper_method :show_category_name
+
+  def show_language_name(language_number)
+
+    case language_number
+    when 0
+      return "EN-US"
+    else
+      return "PT-BR"
+    end
+  end
+
+  helper_method :show_language_name
+
+
   # GET /sentences
   # GET /sentences.json
   def index
@@ -62,13 +93,13 @@ class SentencesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sentence
-      @sentence = Sentence.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_sentence
+    @sentence = Sentence.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def sentence_params
-      params.require(:sentence).permit(:content, :category, :language)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def sentence_params
+    params.require(:sentence).permit(:content, :category, :language)
+  end
 end
